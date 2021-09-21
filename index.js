c1cd0001103a17c1361509d14452ecce1120dc20
Valerie Russell <vruss14@gmail.com>
2021-09-21 18:16:32 -0400

    created basic script that generates one HTML file as expected

---
+++ index.js
@@ -0,0 +1,90 @@
+const fs = require('fs');
+const inquirer = require('inquirer');
+
+askConfirmation();
+
+function askConfirmation() {
+    inquirer
+        .prompt(
+            {
+                type: 'list',
+                message: 'This command-line interface will assist you in preparing an HTML page with relevant meta tags for SEO. Are you ready to proceed?',
+                name: 'confirmation',
+                choices: ['Yes', 'No']
+            },
+        )
+        .then((answer) => {
+            if(answer.confirmation === 'Yes') {
+                askQuestionSet();
+            }
+            return;
+        })
+        .catch((error) => {
+            if (error.isTtyError) {
+            console.log('The prompt could not be rendered.')
+            } else {
+            console.log('Error: Please exit the CLI and begin again.')
+            }
+        });
+}
+
+function askQuestionSet() {
+    inquirer
+        .prompt([
+            {
+                type: 'input',
+                message: 'What is the title of your webpage? If you can, try to include one of your keywords since your title will be used frequently by search engines.',
+                name: 'title',
+            },
+            {
+                type: 'input',
+                message: 'How would you describe your webpage?',
+                name: 'description',
+            },
+            {
+                type: 'input',
+                message: 'Who is the author of the webpage?',
+                name: 'author',
+            },
+            {
+                type: 'input',
+                message: 'What should the name of the file be?',
+                name: 'file',
+            },
+        ])
+        .then((data) => {
+            console.log(data);
+            const fileName = data.file;
+            fs.writeFile(fileName,
+`<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="author" content="${data.author}">
+    <meta name="description" content="${data.description}">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <!-- CSS -->
+    <!-- <link rel="stylesheet" type="text/css" href="ENTER YOUR CSS URL HERE"> -->
+
+    <title>${data.title}</title>
+
+    <!-- Favicon -->
+    <!-- <link rel="shortcut icon" type="image/jpg" href="ENTER YOUR FAVICON URL HERE"/> -->
+</head>
+<body>
+
+    <!-- <script src="ENTER SCRIPT URL HERE"></script> -->
+</body>
+</html>`, (err) => err ? console.error(err) : console.log("Thank you! Your HTML file has been generated successfully."))
+
+    })
+        .catch((error) => {
+            if (error.isTtyError) {
+            console.log('The prompt could not be rendered.')
+            } else {
+            console.log('Error: Please exit the CLI and begin again.')
+            }
+        });
+}
